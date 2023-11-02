99527ea10b8312ca565bfe12290e449a891bd911
Dmitrii Tcimbal <dtsimbal@gmail.com>
2023-11-03 10:49:57 +1100

    Initial commit

---
+++ src/register_types.h
@@ -0,0 +1,7 @@
+#ifndef GDEXTENSION_REGISTER_TYPES_H
+#define GDEXTENSION_REGISTER_TYPES_H
+
+void initialize_gdextension_types();
+void uninitialize_gdextension_types();
+
+#endif // EXAMPLE_REGISTER_TYPES_H
